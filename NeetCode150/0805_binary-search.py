ccb02bb0de2bf6389c1e63d7c734e86993999857
hyaku <nyoronyoro0327@gmail.com>
2025-08-09 09:40:44 +0900

    20250809

--- NeetCode150/0805_binary-search.py
+++ NeetCode150/0805_binary-search.py
@@ -13,9 +13,18 @@ class Solution:
         if nums[m] < target:
             ## ターゲットがmの右側にある場合
             return self.binary_search(m + 1, r, nums, target)
+        ## ターゲットがmの左側にある場合
         return self.binary_search(l, m - 1, nums, target)
 
     ## シンプルなインターフェースを提供するためのメソッド
     ### 利用者はlとrを考える必要がないことが、この設計の利点
     def search(self, nums: List[int], target: int) -> int:
         return self.binary_search(0, len(nums) - 1, nums, target)
+
+### 計算量
+# - 時間計算量：O(log n)
+#   - n = 8の配列で何回比較するか数える
+#     - 最大4回 ≈ log₂(8) + 1
+# - 空間計算量：O(log n)
+#   - 再帰のスタックを数える
+#     - 最大3単位 ≈ log₂(8)
