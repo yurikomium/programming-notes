cc2138244e92c1468baa933d8a095d037ec3b11d
hyaku <nyoronyoro0327@gmail.com>
2025-08-11 08:10:18 +0900

    20250811

--- NeetCode150/0805_binary-search.py
+++ NeetCode150/0805_binary-search.py
@@ -1,4 +1,4 @@
-## 反復二分探索
+## 反復二分探索 ★★覚える★★
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         l, r = 0, len(nums) - 1
@@ -14,6 +14,7 @@ class Solution:
             else:
                 return m
         return -1
+        
 ### 計算量
 # - 時間計算量：O(log n)
 #   - n = 8の配列で何回比較するか数える
@@ -23,7 +24,7 @@ class Solution:
 #     - 3単位
 
 
-## 再帰的二分探索
+## 再帰的二分探索 ★★余力があれば差別化要因になる★★
 
 class Solution:
     def binary_search(self, l: int, r: int, nums: List[int], target: int) -> int:
@@ -51,5 +52,32 @@ class Solution:
 #   - n = 8の配列で何回比較するか数える
 #     - 最大4回 ≈ log₂(8) + 1
 # - 空間計算量：O(log n)
-#   - 再帰のスタックを数える
+#   - 再帰のスタック（再帰が何層に重なるか）を数える <- 再帰では前の関数が終了していないため
 #     - 最大3単位 ≈ log₂(8)
+
+## Upper Bound（上級問題レベル）
+### 「targetと一致する要素を見つけること」ではなく、「targetより大きい最初の要素のインデックス（＝境界位置）を見つけること」が目的
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums)
+
+        while l < r:
+            m = l + ((r - l) // 2)
+            if nums[m] > target:
+                r = m
+            elif nums[m] <= target:
+                l = m + 1
+        return l - 1 if (l and nums[l - 1] == target) else -1
+        ## targetが見つかる場合は l - 1
+        ## targetが存在しない場合・targetが配列の最小値より小さい場合は -1
+        ## l = 0 もしくは nums[l - 1] != target の場合は、if文の結果がFalse。それ以外はTrue
+
+### 計算量
+# - 時間計算量：O(log n)
+#   - n = 8の配列で何回比較するか数える
+#     - 最大4回 ≈ log₂(8) + 1a
+# - 空間計算量：O(1)
+#   - 変数l, r, mを数える
+#     - 3単位
+#   - 再帰を使わないため、スタックは使わない
