e53bbedacd18a11837b1267ca24f1784b309f416
hyaku <nyoronyoro0327@gmail.com>
2025-08-28 06:36:56 +0900

    20250828

--- NeetCode150/20250818_invert-binary-tree.py
+++ NeetCode150/20250818_invert-binary-tree.py
@@ -5,6 +5,8 @@
 #         self.left = left   # TreeNode または None
 #         self.right = right   # TreeNode または None
 
+
+## BFS（幅優先探索）を用いた二分木の反転
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]: # TreeNode型 または None
         if not root: ## 空の木が入力される場合を想定しておく
@@ -20,3 +22,15 @@ class Solution:
             # left, rightどちらも存在する場合は両方をキューに追加する。
             ## 1回のループで1つのノードしか処理できない。自分の直接の子だけ入れ替える。
         return root # 中身は反転済みの木を返す
+
+## 再帰的なアプローチによる二分木の反転
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root: return None
+
+        root.left, root.right = root.right, root.left
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root
