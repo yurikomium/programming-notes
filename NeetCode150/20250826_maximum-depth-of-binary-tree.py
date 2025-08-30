5295a7314ae1cde4952a2f7c5918fd1eeb6e41bc
hyaku <nyoronyoro0327@gmail.com>
2025-08-30 09:11:39 +0900

    20250830

--- NeetCode150/20250826_maximum-depth-of-binary-tree.py
+++ NeetCode150/20250826_maximum-depth-of-binary-tree.py
@@ -1,6 +1,6 @@
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
-            return 0
+            return 0 ## 空のときはreturnの段階で1を返せるよう、ここでは"return 0"にする（Noneではない）
         ## 葉ノードの場合1を返す
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right)) ## 再帰のときはselfをつけるのを忘れずに！
