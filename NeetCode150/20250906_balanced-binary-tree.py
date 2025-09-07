c352dacfb300e1cbee9c21439f8f24a72a0b3c05
hyaku <nyoronyoro0327@gmail.com>
2025-09-08 08:06:00 +0900

    20250908

--- NeetCode150/20250906_balanced-binary-tree.py
+++ NeetCode150/20250906_balanced-binary-tree.py
@@ -18,7 +18,9 @@ class Solution:
             left, right = dfs(root.left), dfs(root.right)
             # [0] = balanced(T/F), [1] = height
             # 現在のノードにおける高さ差チェック
+            ## absは絶対値を求める
             balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1 # 全てTrueのときのみTrueを返す
+            # 一度Falseになったら、以降はずっとFalseを返す
             return [balanced, 1 + max(left[1], right[1])]
 
         return dfs(root)[0]
