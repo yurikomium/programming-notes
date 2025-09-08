132b03abca06419d4ca511245a55204c948be97c
hyaku <nyoronyoro0327@gmail.com>
2025-09-09 08:37:27 +0900

    20250909

--- NeetCode150/20250906_balanced-binary-tree.py
+++ NeetCode150/20250906_balanced-binary-tree.py
@@ -11,9 +11,11 @@ Depth First Search
 
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        # メイン関数はT/Fだけ欲しいが、計算には高さも必要なので、補助関数を実装する
         def dfs(root):
+            # 空ノードの処理を入れておかないと、NoneTypeのエラーになる
             if not root:
-                return [True, 0]
+                return [True, 0] # 存在しないノードはバランスが取れているとみなすのでTrue
 
             left, right = dfs(root.left), dfs(root.right)
             # [0] = balanced(T/F), [1] = height
@@ -21,6 +23,6 @@ class Solution:
             ## absは絶対値を求める
             balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1 # 全てTrueのときのみTrueを返す
             # 一度Falseになったら、以降はずっとFalseを返す
-            return [balanced, 1 + max(left[1], right[1])]
+            return [balanced, 1 + max(left[1], right[1])] # 現在のノードの分の高さを追加する
 
         return dfs(root)[0]
