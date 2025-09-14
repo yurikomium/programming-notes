2e39ea3a5626ff511fcd3051b357cbc1381d31d4
hyaku <nyoronyoro0327@gmail.com>
2025-09-15 08:04:11 +0900

    20250915

--- NeetCode150/20250913_subtree-of-another-tree.py
+++ NeetCode150/20250913_subtree-of-another-tree.py
@@ -6,21 +6,27 @@
 #         self.right = right
 
 class Solution:
-
+    # rootの中でsubRootと同じ部分木があるかどうか（探索）
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        ## どんな木でも空の木は常に部分木に含まれる（何も探さなくていい）
+        ### プログラムがクラッシュしないように追加しているエッジケース
         if not subRoot:
             return True
+        ## 「探索対象（root）が空なのに、何かを探している」→見つからない→False
         if not root:
             return False
-
+        ## rootの現在のノードから始まる部分木がsubRootと同じか？
         if self.sameTree(root, subRoot):
             return True
+        ## rootの左部分木、右部分木にsubRootと同じ部分木があるか？
         return (self.isSubtree(root.left, subRoot) or
                self.isSubtree(root.right, subRoot))
 
+    # 2つの木が完全に同じ構造・値かを判定する（比較）
     def sameTree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
         if not root and not subRoot:
             return True
+        ## 値が同じなら、左の子同士・右の子同士も比較
         if root and subRoot and root.val == subRoot.val:
             return (self.sameTree(root.left, subRoot.left) and
                    self.sameTree(root.right, subRoot.right))
