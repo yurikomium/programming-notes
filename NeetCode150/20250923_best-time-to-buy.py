684594a073bd6a73fdfa1a7b7df4d909e6cb4886
hyaku <nyoronyoro0327@gmail.com>
2025-09-25 06:42:49 +0900

    20250925

--- NeetCode150/20250923_best-time-to-buy.py
+++ NeetCode150/20250923_best-time-to-buy.py
@@ -8,10 +8,32 @@ Space Complexity: O(1)
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        res = 0
+        res = 0 # 取引しないという選択肢（利益=0）を許可している
         for i in range(len(prices)):
+            # 先に買う日を固定
             buy = prices[i]
-            for j in range(i + 1, len(prices)):
+            for j in range(i + 1, len(prices)): # iより後ろしか確認しない
                 sell  = prices[j]
                 res = max(res, sell - buy)
-        return res
+        return 
+        
+"""
+Two Pointers
+
+Time Complexity: O(n) - 価格リストを一度走査するため
+Space Complexity: O(1) - 追加の空間を使用しないため
+
+"""
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l, r = 0, 1
+        maxP = 0
+
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                maxP = max(maxP, profit)
+            else:
+                l = r
+            r += 1
+        return maxP
