e60d446a49425ac259868672eb9acd90fa8512f4
hyaku <nyoronyoro0327@gmail.com>
2025-10-07 08:00:08 +0900

    20251007

--- NeetCode150/20251002_reverse-linked-list.py
+++ NeetCode150/20251002_reverse-linked-list.py
@@ -13,7 +13,7 @@ class Solution:
         prev, curr = None, head
 
         while curr:
-            temp = curr.next ## curr.nextを書き換える前に、現状のnextを退避しておく
+            temp = curr.next ## curr.nextを書き換える前に、現状のnext以降を退避しておく
             curr.next = prev ## 矢印を反転
             prev = curr ## 反転済みリストの先頭はcurrに更新
             curr = temp ## 退避しておいた元のnextに進む
