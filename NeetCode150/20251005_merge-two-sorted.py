41cc695a2095e7d67de7f13a9a0fc4db4bc77d81
hyaku <nyoronyoro0327@gmail.com>
2025-10-11 08:29:11 +0900

    20251011

--- NeetCode150/20251005_merge-two-sorted.py
+++ NeetCode150/20251005_merge-two-sorted.py
@@ -5,18 +5,20 @@ Iteration
 
 class Solution:
     def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
-        ## ダミーノードを置く
+        ## ダミーノードを置く（結果リストの先頭）
         dummy = node = ListNode() ### 新しいノードを作成し、nodeとdummyが同じノードを指すようにする
+        ### nodeは結果リストの現在の末尾（毎回動く）
 
         while list1 and list2:
+            ## list1とlist2の先頭を比較し、小さいほうのnodeを結果リストに追加
             if list1.val < list2.val:
-                node.next = list1
+                node.next = list1 ### 末尾に接続
                 list1 = list1.next
             else:
                 node.next = list2
                 list2 = list2.next
             node = node.next
 
-        node.next = list1 or list2
+        node.next = list1 or list2 ### どちらかがNoneになったらループ終了。残っているほうをそのまま接続
 
         return dummy.next
